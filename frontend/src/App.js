84177f520abcbe1ec49de4cda8df30344b4416c5
dori-dev <mr.dori.dev@gmail.com>
2023-05-23 21:48:42 +0330

    initialize project

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+
+const App = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <main className="my-5">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ullam
+          neque tempora ipsum, dolor ipsam ad odio, molestiae, provident cumque
+          deleniti voluptatum eveniet. Aliquam ea qui, harum sit labore tempora.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ullam
+          neque tempora ipsum, dolor ipsam ad odio, molestiae, provident cumque
+          deleniti voluptatum eveniet. Aliquam ea qui, harum sit labore tempora.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ullam
+          neque tempora ipsum, dolor ipsam ad odio, molestiae, provident cumque
+          deleniti voluptatum eveniet. Aliquam ea qui, harum sit labore tempora.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ullam
+          neque tempora ipsum, dolor ipsam ad odio, molestiae, provident cumque
+          deleniti voluptatum eveniet. Aliquam ea qui, harum sit labore tempora.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ullam
+          neque tempora ipsum, dolor ipsam ad odio, molestiae, provident cumque
+          deleniti voluptatum eveniet. Aliquam ea qui, harum sit labore tempora.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ullam
+          neque tempora ipsum, dolor ipsam ad odio, molestiae, provident cumque
+          deleniti voluptatum eveniet. Aliquam ea qui, harum sit labore tempora.
+        </p>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,83 @@
+const Footer = () => {
+  return ( <>
+    <footer className="bg-light text-center text-lg-start mt-5">
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-lg-3 col-md-6 mb-0 mb-md-4">
+            <h5 className="text-uppercase">Shop</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="#!" className="text-dark">link 1</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 2</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 3</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 4</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-0 mb-md-4">
+            <h5 className="text-uppercase mb-0">Products</h5>
+            <ul className="list-unstyled mt-2">
+              <li>
+                <a href="#!" className="text-dark">link 1</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 2</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 3</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 4</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Blog</h5>
+            <ul className="list-unstyled mb-0">
+              <li>
+                <a href="#!" className="text-dark">link 1</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 2</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 3</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 4</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase mb-0">About</h5>
+            <ul className="list-unstyled mt-2">
+              <li>
+                <a href="#!" className="text-dark">link 1</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 2</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 3</a>
+              </li>
+              <li>
+                <a href="#!" className="text-dark">link 4</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
+         © E-Shop 2023
+      </div>
+    </footer></> );
+}
+ 
+export default Footer;
+
