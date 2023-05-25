5f1588431e7ccf887c1844ee98b285cec1a5cca4
dori-dev <mr.dori.dev@gmail.com>
2023-05-25 15:29:12 +0330

    add first view

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+
+class Hello(View):
+    def get(self, request):
+        return JsonResponse({
+            'msg': "Hello, World!",
+        })
