fd0f1a8b1558bd6decb15b8ffae2fa4374dd33b8
dori-dev <mr.dori.dev@gmail.com>
2023-05-26 10:46:30 +0330

    add order models

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+admin.site.register(models.Product)
+admin.site.register(models.Review)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
+admin.site.register(models.ShippingAddress)
