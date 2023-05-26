2f02fec7dc3eb95d2eddf9e4bc61a70134d48d59
dori-dev <mr.dori.dev@gmail.com>
2023-05-26 10:46:37 +0330

    add media root & url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView,
 )
@@ -11,3 +13,5 @@ urlpatterns = [
     path('redoc/', SpectacularRedocView.as_view(url_name='schema')),
     path('api/v1/', include('base.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
