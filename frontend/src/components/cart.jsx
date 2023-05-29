10f57e3a26f25514ef395e069d492d62bf533ed4
dori-dev <mr.dori.dev@gmail.com>
2023-05-29 13:58:41 +0330

    use addToCart action in cart

--- frontend/src/components/cart.jsx
+++ frontend/src/components/cart.jsx
@@ -1,13 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { addToCart } from "../actions/cartActions";
+import { useDispatch } from "react-redux";
 
 const Cart = () => {
-  const { id, quantity } = useParams();
+  const { id } = useParams();
+  const { search } = useLocation();
+  let query = {};
+  const dispatch = useDispatch();
+  search
+    .slice(1)
+    .split("&")
+    .forEach((item) => {
+      let [key, value] = item.split("=");
+      query[key] = value;
+    });
+  const quantity = query["quantity"] || 1;
+  useEffect(() => {
+    if (id && quantity) {
+      dispatch(addToCart(id, quantity));
+    }
+  }, [id, quantity, dispatch]);
   return (
     <>
       <h1>Cart</h1>
-      <p>{id}</p>
-      <p>{quantity}</p>
-      <div style={{ marginBottom: "1000px" }}></div>
     </>
   );
 };
