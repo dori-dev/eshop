8d4d7e7da37a13ee8985f8c635ec0d7d405fb3c8
dori-dev <mr.dori.dev@gmail.com>
2023-06-11 20:31:45 +0330

    remove product from base

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
-from . import models
-
-
-admin.site.register(models.Product)
-admin.site.register(models.Review)
-admin.site.register(models.Order)
-admin.site.register(models.OrderItem)
-admin.site.register(models.ShippingAddress)
+# Register your models here.
--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,28 +1,4 @@
-from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-from base import models
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
-    class Meta:
-        model = models.Product
-        fields = '__all__'
-        read_only_fields = [
-            'user',
-            'id',
-            'rating',
-            'reviews_count',
-        ]
-
-    def get_image(self, product: models.Product):
-        request = self.context.get('request')
-        if request:
-            image_url = product.image.url
-            return request.build_absolute_uri(image_url)
-        return product.image.url
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
