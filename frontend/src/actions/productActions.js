97bd8f1586f7b98d9563329e1395acc2e758fc7e
dori-dev <mr.dori.dev@gmail.com>
2023-06-13 22:00:52 +0330

    fix endpoint url

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const fetchProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST.REQUEST });
-    const { data } = await axios.get("/api/v1/products/");
+    const { data } = await axios.get("/api/v1/products/list/");
     dispatch({
       type: PRODUCT_LIST.SUCCESS,
       payload: data,
@@ -23,7 +23,7 @@ export const fetchProducts = () => async (dispatch) => {
 export const fetchProduct = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAIL.REQUEST });
-    const { data } = await axios.get(`/api/v1/product/${id}/`);
+    const { data } = await axios.get(`/api/v1/products/${id}/`);
     dispatch({
       type: PRODUCT_DETAIL.SUCCESS,
       payload: data,
