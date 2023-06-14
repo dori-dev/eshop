679d8f60e261274f1b876d249b8d7885b55ce29e
dori-dev <mr.dori.dev@gmail.com>
2023-06-14 13:30:34 +0330

    add user to initial state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,17 +10,22 @@ import {
   detailProductReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   listProduct: listProductReducer,
   detailProduct: detailProductReducer,
   cart: cartReducer,
+  user: userLoginReducer,
 });
 
 const cartItems = localStorage.getItem("cartItems");
+const userInfo = localStorage.getItem("userInfo");
 const initialState = {
   cart: { cartItems: cartItems ? JSON.parse(cartItems) : [] },
+  user: { userInfo: userInfo ? JSON.parse(userInfo) : [] },
 };
+
 const middleware = [thunk];
 const store = createStore(
   reducer,
