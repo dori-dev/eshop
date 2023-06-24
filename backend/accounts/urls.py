ba4f51723945977fafa1442b3c4e50a0d8a24b47
dori-dev <mr.dori.dev@gmail.com>
2023-06-24 16:56:25 +0330

    use throttle for token refresh view

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,
     TokenVerifyView,
 )
 
@@ -10,7 +9,7 @@ from accounts import views
 
 token = [
     path('', TokenObtainPairView.as_view()),
-    path('refresh/', TokenRefreshView.as_view()),
+    path('refresh/', views.CustomTokenRefreshView.as_view()),
     path('verify/', TokenVerifyView.as_view()),
 ]
 
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,7 +135,17 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.ScopedRateThrottle',
+        # 'rest_framework.throttling.AnonRateThrottle',
+        # 'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'refresh_token': '2/minute',
+        # 'anon': '10/minute',
+        # 'user': '20/minute',
+    }
 }
 
 SPECTACULAR_SETTINGS = {
