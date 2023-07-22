fbe5452c275f7257de8a9bd8ea9817290973202d
dori-dev <mr.dori.dev@gmail.com>
2023-07-22 20:50:36 +0330

    Fixed footer on bottom

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return ( <>
-    <footer className="bg-light text-center text-lg-start mt-5">
+    <footer className="bg-light text-center text-lg-start mt-5 fixed-bottom">
       <div className="container p-4">
         <div className="row">
           <div className="col-lg-3 col-md-6 mb-0 mb-md-4">
