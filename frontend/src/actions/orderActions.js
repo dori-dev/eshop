3352024109e2c003b7e394b74cba01bd3d558774
dori-dev <mr.dori.dev@gmail.com>
2023-07-26 07:13:51 +0330

    Add get order details action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE } from "../constants/orderConstants";
+import { ORDER_CREATE, ORDER_DETAILS } from "../constants/orderConstants";
 import { CART_ITEM } from "../constants/cartConstants";
 
 import axiosInstance from "../utils/axiosInstance";
@@ -21,6 +21,26 @@ export const createOrder = (order) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: ORDER_CREATE.FAIL,
+      payload: getErrorMessage(error),
+    });
+  }
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_DETAILS.REQUEST });
+    const {
+      user: { userInfo },
+    } = getState();
+    const authRequest = axiosInstance(userInfo, dispatch);
+    const { data } = await authRequest.get(`/api/v1/orders/${id}/`);
+    dispatch({
+      type: ORDER_DETAILS.SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS.FAIL,
       payload: getErrorMessage(error),
     });
   }
